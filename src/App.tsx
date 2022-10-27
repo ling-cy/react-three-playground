0ef0894bf4b1f49f51fa95a0408e0875e120aeba
ling-cy <lingcy.work@gmail.com>
2022-10-27 23:08:16 +0800

    feat: basic setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from 'react';
+import { Example1 } from './components';
+
+const EXAMPLE_MAP: Record<string, React.ComponentType> = {
+  'Example 1': Example1,
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [componentName, setComponentName] = useState('Example 1');
+
+  const Component = EXAMPLE_MAP[componentName];
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div style={{ width: '100%', height: '100%' }}>
+      <select onChange={(e) => setComponentName(e.target.value)}>
+        {Object.keys(EXAMPLE_MAP).map((name) => (
+          <option key={name} value={name}>
+            {name}
+          </option>
+        ))}
+      </select>
+      <Component />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
